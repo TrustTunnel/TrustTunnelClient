123571f11a8e6e84a520f36d20f354efaade5fa2
Sergey Fionov <sfionov@adguard.com>
2024-09-25 20:22:40 +0300

    Minus one move

--- core/src/vpn_manager.cpp
+++ core/src/vpn_manager.cpp
@@ -687,13 +687,12 @@ VpnDnsUpstreamValidationStatus vpn_validate_dns_upstream(const char *address) {
 bool vpn_process_client_packets(Vpn *vpn, VpnPackets packets) {
     std::unique_lock l(vpn->stop_guard);
 
-    auto packets_holder = std::make_shared<VpnPacketsHolder>(packets);
-
     if (!vpn_get_event_loop(vpn)) {
+        auto packets_holder = std::make_shared<VpnPacketsHolder>(packets);
         return false;
     }
 
-    vpn->submit([vpn, packets_holder = std::move(packets_holder)]() mutable {
+    vpn->submit([vpn, packets_holder = std::make_shared<VpnPacketsHolder>(packets)]() mutable {
         auto packets = packets_holder->release();
         vpn->client.process_client_packets({packets.data(), (uint32_t) packets.size()});
     });
