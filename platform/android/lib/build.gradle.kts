9c732fe2984e607c0e2215aa0ea890f8a4d304f7
Bamboo <Bamboo>
2026-04-28 19:10:32 +0000

    skipci: Automatic version increment by Bamboo

--- platform/android/lib/build.gradle.kts
+++ platform/android/lib/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("maven-publish")
 }
 
-version = "1.0.57"
+version = "1.0.58"
 
 android {
     namespace = "com.adguard.trusttunnel"
--- trusttunnel/setup_wizard/src/version.rs
+++ trusttunnel/setup_wizard/src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "1.0.57";
+pub const VERSION: &str = "1.0.58";
