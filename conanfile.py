c4fd6e3dabba9d1144a5398f8c3ba04c39b1e139
Ilia Zhirov <i.zhirov@adguard.com>
2026-06-08 21:48:12 +0500

    AG-53110 add diagnostic logging for quic http3 tls certificate verification failures in corelibs (#10)
    * Improve TLS certificate verification diagnostic logging
    
    - Add tls_get_cert_diagnostic_info() helper returning Subject, Issuer
      and chain length for a given certificate
    - Add warn-level logging to Http3/Http2 verify callbacks (previously
      silent on failure) with hostname and cert diagnostic info
    - Promote cert verification failure logs from dbg to warn in
      close_session_inner and TCP_SOCKET_EVENT_ERROR handler
    - Log TLS handshake failure flag on QUICHE_ERR_TLS_FAIL
    - Add hostname, IP and cert diagnostic context to vpn_manager
      ssl_verify_callback error messages
    
    * Promote cert diagnostic logs to warn/err level
    
    * Remove trace logs for successful cert verification
    
    * Extract common cert verification logic into helper
    
    * Use ag::tls::get_cert_diagnostic_info from native-libs-common
    
    * Demote cert verification failure logs from err to warn
    
    * Inline cert_verify_failed context into quiche error log

--- conanfile.py
+++ conanfile.py
@@ -31,8 +31,8 @@ class VpnLibsConan(ConanFile):
     exports_sources = patch_files
 
     def requirements(self):
-        self.requires("dns-libs/2.8.51@adguard/oss", transitive_headers=True)
-        self.requires("native_libs_common/8.1.28@adguard/oss", transitive_headers=True)
+        self.requires("dns-libs/2.8.53@adguard/oss", transitive_headers=True)
+        self.requires("native_libs_common/8.1.33@adguard/oss", transitive_headers=True)
 
         self.requires("brotli/1.1.0", transitive_headers=True)
         self.requires("cxxopts/3.1.1", transitive_headers=True)
